d9bd34fe22cd4e95083678b2c4b24804724adfe5
Prithvee Raj Singh <prithvee11901@gmail.com>
2023-07-17 19:50:05 +0530

    [C/C++] Misc. Changes..

---
+++ C-Code/Basics/1-Hello-World.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// This is Called A Comment..
+
+/*
+@brief: The Above #include statement is used to import functions from system..
+@note:  Currently, We've Imported Standard Input/Output Header File.. That allows us to use printf 
+        and Similar functions...
+*/
+
+int main(int argc, char const *argv[])
+{
+    
+    printf("Hello, World!-\n");
+    return 0;
+}
