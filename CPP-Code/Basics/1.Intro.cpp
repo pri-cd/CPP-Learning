e2c8d4b7ed88886fc685ec29b7579f8b89f09829
Prithvee Raj Singh <prithvee11901@gmail.com>
2023-08-13 18:28:45 +0530

    [CPP] Addition of C++ Repo!

---
+++ CPP-Code/Basics/1.Intro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* @brief: This is main() and the execution of a binary Starts from Here!-- */
+
+int main(int argc, char **argv)
+{
+    int returnCode = EXIT_SUCCESS;
+    /* @brief: This is a Variable! called "returnCode". */
+    /* @brief: This is a C++ Variant of printf() function. Called cout */
+    cout << "Hello, World!" << endl;
+
+    /* @note: In Case of not including "using namespace std;" we'll have to do "std::cout" */
+    return returnCode;
+} 
---
+++ CPP-Code/Basics/4.Syntax.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/* @brief; Preprocessor Directive: Used To Include Files. */
+
+int main(int argc, char const *argv[])
+/* This is Main() This is Where The Execution Starts From!- */
+{
+    /* cout ---- Belongs To "std" Namespace*/
+    std::cout << "Hello, World!" << std::endl ;     
+    /* This is Used To Print on STDOUT Stream! */
+    /* "std::endl" is used to end a line in Cpp Code, we Could Also Use \n For That Purpose! */
+    return 0;
+    /* This Returns The Error Code: 0(Zero) Generally Means Success! */
+}
+
+/* Program Should Be Written In Curly, Braces! As Curly Braces Defines The Scope Of 
+   Almost Everything in CPP/C Code Except Global Variables. */
